9d7e8fcee0f30ad260b09e67d405ba02c86cff31
Lorrany <lorrany.acopiara@gmail.com>
2026-04-08 10:41:58 -0300

    verificações de inputs

--- confirmacaoDeDados.js
+++ confirmacaoDeDados.js
@@ -1,24 +1,111 @@
+/* Função feita para o arquivo redefinirSenha.html.
+    A função garante que o usuario preencha os campos, não escreva 
+    duas senhas diferente, e precise escrever uma senha com 8 caracteres ou mais.*/
 function confirmarSenha() {
-    const senha = document.getElementById('senha').value;
-    const senha2 = document.getElementById('confirmeSenha').value;
+    const senha = document.getElementById('senha');
+    const senha2 = document.getElementById('confirmeSenha');
 
-    if(senha === senha2){
+    if(!senha.value || !senha2.value){
+        const caixa = document.getElementById('container');
+        caixa.style.height = '36rem';
+
+        document.getElementById('aviso').innerHTML = `
+            <p>Preencha todos os campos!</p>
+        `
+        if(!senha.value){
+            senha.classList.add('erro');
+            senha.onclick = function() {
+                senha.classList.remove('erro');
+                senha.onclick = null;
+            }
+        }
+        if(!senha2.value){
+            senha2.classList.add('erro');
+            senha2.onclick = function() {
+                senha2.classList.remove('erro');
+                senha2.onclick = null;
+            }
+        }
+    } else if (senha.value.length < 8 || senha2.value.length < 8){
+        const caixa = document.getElementById('container');
+        caixa.style.height = '36rem';
+
+        document.getElementById('aviso').innerHTML = `
+            <p>Escreva no minimo 8 caracteres</p>
+        `
+        senha.classList.add('erro');
+        senha.onclick = function() {
+            senha.classList.remove('erro');
+            senha.onclick = null;
+        }
+        senha2.classList.add('erro');
+        senha2.onclick = function() {
+            senha2.classList.remove('erro');
+            senha2.onclick = null;
+        }
+
+    } else if(senha.value === senha2.value){
         return window.location.href = "confirmacao.html";
     } else {
-        alert("As senhas não estão iguais! Tente novamente!")
+        const caixa = document.getElementById('container');
+        caixa.style.height = '36rem';
+
+        document.getElementById('aviso').innerHTML = `
+            <p>As senhas não são iguais!</p>
+        `
+        senha.classList.add('erro');
+        senha.onclick = function() {
+            senha.classList.remove('erro');
+            senha.onclick = null;
+        }
+        senha2.classList.add('erro');
+        senha2.onclick = function() {
+            senha2.classList.remove('erro');
+            senha2.onclick = null;
+        }
     }
 }
 
+/* Função feita para o arquivo esqueceuSenha.html
+    A função garante que o usuario preencha o campo e não escreva um email inválido*/
 function checarEmail(){
-    const email = document.getElementById('email').value;
+    const email = document.getElementById('email');
 
-    if(!email){
-        alert("Você não preencheu o campo do email!")
+    if(!email.value){
+        const caixa = document.getElementById('container');
+        caixa.style.height = '30rem';
+
+        document.getElementById('aviso').innerHTML = `
+            <p>Preencha o campo de email!</p>
+        `
+        if(!email.value){
+            email.classList.add('erro');
+            email.onclick = function() {
+                email.classList.remove('erro');
+                email.onclick = null;
+            }
+        }
+    } else if(!email.value.includes('@') || !email.value.includes('.') || email.value.indexOf('.') < email.value.indexOf('@') + 2) {
+        const caixa = document.getElementById('container');
+        caixa.style.height = '30rem';
+
+        document.getElementById('aviso').innerHTML = `
+            <p>Email inválido!</p>
+        `
+        if(!email.value){
+            email.classList.add('erro');
+            email.onclick = function() {
+                email.classList.remove('erro');
+                email.onclick = null;
+            }
+        }
     } else {
         return window.location.href = "redefinirSenha.html";
     }
 }
 
+// Função feita para o arquivo cadastro.html
+// preciso refazer
 function carregarGeneros() {
     const generos = [
         "Mulher cis",
@@ -39,6 +126,9 @@ function carregarGeneros() {
     });
 }
 
+/* Função feita para o arquivo cadastro.html
+    A função garante que todos os campos do cadastro sejam preenchidos
+    mas ainda não possui algumas verificações importantes.*/
 function avisar() {
     const senha = document.getElementById('senha');
     const senha2 = document.getElementById('confirmeSenha');
@@ -118,7 +208,7 @@ function avisar() {
         <hr>
         <p>Volte e faça seu login.</p>
         <div class="botaoDeAviso">
-        <a href="index.html"><button type="button" id="botaoV">Voltar</button></a>
+        <a href="index.html"><button type="button">Voltar</button></a>
         </div>
         `
         caixa.classList.add('avisoConclusao');
